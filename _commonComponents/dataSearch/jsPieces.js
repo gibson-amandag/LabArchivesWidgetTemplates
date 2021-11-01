cb57601b64a0d4aba8446e9a867896bf96b1005b
Amanda Gibson <gibsonag@umich.edu>
2021-10-31 21:09:38 -0400

    Add explanation for dataSearch

--- _commonComponents/dataSearch/jsPieces.js
+++ _commonComponents/dataSearch/jsPieces.js
@@ -1,4 +1,13 @@
 my_widget_script = {
+    /**
+     * Build a string for search for a HTML element that has a particular data value
+     * ex: mouseSearch = dataSearch("mouse", 1) -> mouseSearch = "[data-mouse=1]"
+     * $(".mouseID" + mouseSearch) would find an element with class mouseID and data-mouse = 1
+     * 
+     * @param {*} dataName String for the name of the data attribute within the HTML form 
+     * @param {*} dataValue Value you want to match
+     * @returns A text string that can be added to a jQuery search for an element that matches the data value
+     */
     dataSearch: function (dataName, dataValue) {
         var dataSearch = "[data-" + dataName + "='" + dataValue + "']";
         return dataSearch
