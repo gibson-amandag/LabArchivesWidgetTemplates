f9defcbd8f147920a0918b7a5b61cce512496787
Amanda Gibson <gibsonag@umich.edu>
2021-10-31 21:10:10 -0400

    Add makeCard within js

---
+++ _commonComponents/makeCardInJS/jsPieces.js
@@ -0,0 +1,72 @@
+my_widget_script =
+{
+    initDynamicContent: function (parsedJson) {
+        // for (var i = 0; i < parsedJson.addedRows; i++) {
+        // };
+    },
+
+    adjustForMode: function (mode) {
+        if (mode !== "edit" && mode !== "edit_dev") {
+            //disable when not editing
+            $(".disableOnView").prop("disabled", true);
+        }
+    },
+    
+    addEventListeners: function () {
+        
+    },
+
+    setUpInitialState: function () {
+        
+
+        my_widget_script.resize();
+    },
+    
+    resize: function () {
+        //resize the container
+        my_widget_script.parent_class.resize_container();
+    },
+    // ********************** END CUSTOM INIT METHODS **********************
+
+
+    // ********************** START CUSTOM TO_JSON METHODS **********************
+    getDynamicContent: function () {
+        var dynamicContent = {};
+        return dynamicContent;
+    },
+    // ********************** END CUSTOM TO_JSON METHODS **********************
+
+    toggleCard: function ($cardHead) {
+        // console.log($cardHead.next());
+        $cardHead.next().toggleClass("collapse");
+        $cardHead.next().find("textarea.autoAdjust").each(function () {
+            if(! $(this).is(":hidden")) {
+                this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+            } 
+        });
+        my_widget_script.resize();
+    },
+
+    makeCard: function ($div, cardHeadContent, cardBodyContent) {
+        // Add extras to header, such as classes or data attributes in calling function after making the card
+        $div.append(
+            $("<div/>", {
+                "class": "card"
+            }).append(
+                $("<button></button>", {
+                    "type": "button",
+                    "class": "card-header",
+                }).on("click", function () {
+                    my_widget_script.toggleCard($(this));
+                }).append(cardHeadContent)
+            ).append(
+                $("<div/>", {
+                    "class": "card-body collapse"
+                }).append(
+                    cardBodyContent
+                )
+            )
+        )
+        my_widget_script.resize();
+    },
+};
